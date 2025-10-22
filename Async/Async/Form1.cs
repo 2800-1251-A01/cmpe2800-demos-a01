919f7b2db0004a252ca3d1b05a518cc48c07fe9b
Herb Vanselow <herbv@nait.ca>
2025-10-22 11:03:57 -0600

    Update Form1.cs

--- Async/Async/Form1.cs
+++ Async/Async/Form1.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Specialized;
+using System.Text;
 using static System.Diagnostics.Trace;
 namespace Async
 {
@@ -17,33 +19,87 @@ namespace Async
     async private void _btnStart_Click(object? sender, EventArgs e)
     {
       _btnStart.BackColor = Color.Gray;
-      WriteLine($"_btnStart_Click:Pre-await() Thread: {System.Threading.Thread.CurrentThread.ManagedThreadId}");
+      WriteLine($"_btnStart_Click:Pre-await() Thread: {System.Threading.Thread.CurrentThread.ManagedThreadId} {Environment.TickCount}");
 
       await Task.Delay(4000); // Main Form continues the event loop
+
       // continue execution here after Delay task is complete
-      WriteLine($"_btnStart_Click:Post-await() Thread: {System.Threading.Thread.CurrentThread.ManagedThreadId}");
+      WriteLine($"_btnStart_Click:Post-await() Thread: {System.Threading.Thread.CurrentThread.ManagedThreadId} {Environment.TickCount}");
 
       _btnStart.BackColor = Color.Orange; // So we know we made it here
 
       // The run task need not be async, it will be ( probably ) a long blocking operation
-      await Task.Run(() => AnotherDelay()); // invoke async lambda method
-      await Task.Run(AnotherDelay);
-      // continue execution here after AnotherDelay() has completed
-      WriteLine($"_btnStart_Click:Post-await-await() Thread: {System.Threading.Thread.CurrentThread.ManagedThreadId}");
+      //   Check the AnotherDelay() output, clearly these .Run() calls spawn threads for AnotherDelay calls
+      // Direct call using Action<> deduction, no args required.
+      await Task.Run(BasicDelay);
+      WriteLine($"_btnStart_Click:BasicDelay Done {Environment.TickCount}");
 
-      //finally
+      // Non-action function required!, thats OK, just use a lambda, and variable capture
+      int delay = 1500;
+      await Task.Run(() => AnotherDelay(delay)); // invoke async lambda method
+      WriteLine($"_btnStart_Click:AnotherDelay Done {Environment.TickCount}");
+
+      // Using a async function that returns a Task can be awaited, this is what
+      //  most XXXAsync functions work like
+      string gotIt = await GetWordAsync("Whirrlygig");
+      WriteLine($"_btnStart_Click:GetWordAsync returned {gotIt} {Environment.TickCount}");
+
+      // continue execution here after AnotherDelay() has completed
+      WriteLine($"_btnStart_Click:Post-await-await() Thread: {System.Threading.Thread.CurrentThread.ManagedThreadId} {Environment.TickCount}");
       _btnStart.BackColor = Color.Pink;
     }
 
+    // NOT async, but will call async function Foo();
+    // NOTE : await within Foo() will pause and this handler is paused, execution resumes in the UI message pump
+    //        until Foo() await completes, and it continues execution
     private void _btnTry_Click(object? sender, EventArgs e)
     {
+      // async function Foo(), it has local awaits, which will pause
+      //  AND then returns execution to the caller
+      Foo(); 
+      // Foo() -> when an await is encountered, execution returns to the CALLER - Right here,
+      //  You will see the button color and output complete BEFORE Foo() completes !!
+      //  Yet Foo() will complete, after this handler has completed !
+      //  LESSON : Don't call async functions that you require completion on
+
+      // NOTE : await Foo(); is illegal because, while it IS async, it returns void, not a Task<> so it can't be awaited.
       _btnTry.BackColor = Color.Violet;
-      Foo();
-      WriteLine($"btnTry_Click:UI Thread: {System.Threading.Thread.CurrentThread.ManagedThreadId}");
+      WriteLine($"btnTry_Click:Foo() complete");
     }
-    private void AnotherDelay()
+    
+    // Simple Action compliant function, no args, not async - could be some long running transform algoritm
+    private void BasicDelay()
     {
       Task.Delay(2000).Wait(); // Same as Sleep(2000) a blocking call
+      WriteLine($"BasicDelay() Thread: {System.Threading.Thread.CurrentThread.ManagedThreadId} {Environment.TickCount}");
+    }
+    
+    // NOT Action compliant, requires an argument to start some long running process, also NOT async ( see StartButton handler )
+    private void AnotherDelay(int delay = 2000)
+    {
+      Task.Delay(delay).Wait(); // Same as Sleep(2000) a blocking call
+      WriteLine($"AnotherDelay() Thread: {System.Threading.Thread.CurrentThread.ManagedThreadId} {Environment.TickCount}");
+    }
+    
+    // Simulated NNNAsync IO method, faked
+    Task<string> GetWordAsync(string source)
+    {
+      // This really does nothing, other than returning the string,
+      //   but shows an alternate to an async function with an await, since this
+      //   does not await, it is not async - but since it returns a Task, it can be awaited..
+      return Task.Run(() => { WriteLine("**"); return source; });
+
+      // THE previous return overrides these following examples
+
+      // while this technically meets the requirements of returning a Task, and it can be
+      //   awaited in the calling code, it NEVER STARTED.. and when awaited WILL NEVER COMPLETE !
+      return new Task<string>(() => { WriteLine("**"); return source; });
+
+      // Solved : You could make the task, start it, and return the task - at least it is started
+      //   Since Run() is a new Task that is started, it is the same thing.
+      var retTask = new Task<string>(() => { WriteLine("**"); return source; });
+      retTask.Start();
+      return retTask;
     }
     async Task<string> GreetAfter(string who, int when)
     {
@@ -61,14 +117,14 @@ namespace Async
       // these are then saved in tasks
       var names = new[] { "John", "Jill", "Jane", "Jake" };
       var tasks = names.Select(x => GreetAfter(x, random.Next(1, 5) * 1000));
-      WriteLine($"Tasks created : {System.Threading.Thread.CurrentThread.ManagedThreadId}");
-      Task.Delay(2000).Wait(); // Wait for 2 seconds
-      WriteLine($"Task.WhenAll() starts tasks : {System.Threading.Thread.CurrentThread.ManagedThreadId}");
+      WriteLine($"{tasks.Count()} Tasks created : Thread : {System.Threading.Thread.CurrentThread.ManagedThreadId}");
+      WriteLine($"Task.WhenAll() about to start : Thread :  {System.Threading.Thread.CurrentThread.ManagedThreadId}");
       var results = await Task.WhenAll(tasks);
       foreach (string item in results)
       {
         WriteLine(Newtonsoft.Json.JsonConvert.DeserializeObject<Person>(item));
       }
+      WriteLine($"Foo(): done");
     }
 
 
